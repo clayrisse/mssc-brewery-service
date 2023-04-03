0b1d4a74974532b6b5d7d889bd3d1da2c11d611d
clayrisse <clayrisse@outlook.com>
2023-04-03 16:31:25 +0200

    Added ExceptionHandler

--- src/main/java/microservice/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/microservice/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,14 +18,31 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null //defensive coding!, these are read only properties we do the in case a malicious actor tries to set this properties
     private UUID id;
+
+    @Null //defensive coding!, these are read only properties we do the in case a malicious actor tries to set this properties
     private Integer version;
+
+    @Null //defensive coding!, these are read only properties we do the in case a malicious actor tries to set this properties
     private OffsetDateTime createDate;
+
+    @Null //defensive coding!, these are read only properties we do the in case a malicious actor tries to set this properties
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @NotNull @Positive
     private Long upc;
+
+    @NotNull @Positive
     private BigDecimal price;
+
     private Integer quantityInHand;
 
 
