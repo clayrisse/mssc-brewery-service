ecab93d11dfb131d6cabe9c8fff801c5dbab6b62
clayrisse <clayrisse@outlook.com>
2023-04-05 21:07:17 +0200

    DateMapper done

--- src/main/java/microservice/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/microservice/msscbeerservice/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import microservice.msscbeerservice.domain.Beer;
 import microservice.msscbeerservice.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
     BeerDto BeerToBeerDto(Beer beer);
     Beer BeerDtoToBeer(BeerDto dto);
